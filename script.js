ea2504061e36330175b147ab4487307a0ae761bc
darrionn33 <darrionn33@gmail.com>
2023-11-20 17:35:26 +0530

    Added slide indicators and a time interval function

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 const nextButton = document.querySelector("button#next");
-const prevButton = document.querySelector("button");
+const prevButton = document.querySelector("button#prev");
 const cards = document.querySelector(".cards");
 
-let minValue = 0;
-let maxValue = getComputedStyle(cards).width.split("px")[0];
-let currentValue = minValue;
+let currentCard = 0;
 
-let scrollValue = maxValue / cards.children.length;
+let scrollValue =
+  getComputedStyle(cards).width.split("px")[0] / cards.children.length;
+
+const setActive = () => {
+  document.querySelectorAll(".indicator").forEach((e, index) => {
+    if (index === currentCard) {
+      e.style.transform = "scale(1.2)";
+    } else {
+      e.style.transform = "scale(0.7)";
+    }
+  });
+  let currentValue = scrollValue * currentCard;
+  cards.style.transform = `translateX(-${currentValue}px)`;
+};
 
 nextButton.onclick = () => {
-  if (currentValue < maxValue - scrollValue) {
-    cards.style.transform = `translateX(-${currentValue + scrollValue}px)`;
-    currentValue += scrollValue;
+  if (currentCard < cards.children.length - 1) {
+    currentCard++;
+    setActive();
   }
 };
+
 prevButton.onclick = () => {
-  if (currentValue > minValue) {
-    cards.style.transform = `translateX(-${currentValue - scrollValue}px)`;
-    currentValue -= scrollValue;
+  if (currentCard > 0) {
+    currentCard--;
+    setActive();
   }
 };
+
+for (let i = 0; i < cards.children.length; i++) {
+  const button = document.createElement("button");
+  button.id = i;
+  button.classList.add("indicator");
+  document.querySelector(".indicator-div").appendChild(button);
+
+  button.onclick = (e) => {
+    currentCard = +e.target.id;
+    setActive();
+  };
+}
+
+setInterval(() => {
+  if (currentCard === cards.children.length - 1) {
+    currentCard = 0;
+  } else {
+    currentCard++;
+  }
+  setActive();
+}, 5000);
+
+setActive();
